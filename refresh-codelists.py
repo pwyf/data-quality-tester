4bdc655ec25dc322dea92266c10e268878a8b101
Andy Lulham <a.lulham@gmail.com>
2017-02-24 12:36:37 +0000

    Load codelists over API instead

--- refresh-codelists.py
+++ refresh-codelists.py
@@ -3,28 +3,35 @@ import os.path
 
 import yaml
 from lxml import etree
+import requests
 
-
-# this should also do a `git submodule update` type thing
 
 # we bundle all codelists together, because we're flexible
 # w.r.t. different versions of the standard.
 
-codelists = {}
-codelist_dirs = ['non-embedded', '1', '2']
+mapping_tmpl = 'http://iatistandard.org/{version}/codelists/downloads/clv2/mapping.json'
+codelist_tmpl = 'http://iatistandard.org/{version}/codelists/downloads/clv2/json/en/{codelist_name}.json'
+versions = ['1.05', '2.02']
 
-for codelist_dir in codelist_dirs:
-    codelist_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'codelists', codelist_dir, 'xml')
-    codelist_filenames = listdir(codelist_path)
-    for codelist_filename in codelist_filenames:
-        codelist_name = codelist_filename.split('.')[0]
-        codelist_xml = etree.parse(os.path.join(codelist_path, codelist_filename))
-        codes = [x.text for x in codelist_xml.xpath('//code')]
+codelists = {}
+
+for version in versions:
+    print('loading mapping version-{version} ...'.format(version=version))
+    version = version.replace('.', '')
+    mapping = requests.get(mapping_tmpl.format(version=version)).json()
+    codelist_names = {x['codelist']: None for x in mapping}.keys()
+    for codelist_name in codelist_names:
+        print('loading codelist {codelist_name} (version-{version}) ...'.format(codelist_name=codelist_name, version=version))
+        codelist = requests.get(codelist_tmpl.format(version=version, codelist_name=codelist_name)).json()
+        if codelist['attributes']['complete'] != '1':
+            continue
+        codes = [entry['code'] for entry in codelist['data']]
         if codelist_name not in codelists:
             codelists[codelist_name] = set()
         codelists[codelist_name].update(codes)
 
 codelists = {codelist_name: list(codelist_set) for codelist_name, codelist_set in codelists.items()}
 
+print('writing codeslists.yaml ...')
 with open('codelists.yaml', 'w') as f:
     yaml.dump(codelists, f)
