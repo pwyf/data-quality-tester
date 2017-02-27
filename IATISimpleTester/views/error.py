2dcc90c8c3efe929381d40976baf9d8bf28ef32b
Andy Lulham <a.lulham@gmail.com>
2017-02-27 11:59:47 +0000

    Scrap nice 500 handling for now

--- IATISimpleTester/views/error.py
+++ IATISimpleTester/views/error.py
@@ -8,9 +8,3 @@ def page_not_found(e):
     if request.path.endswith('.json'):
         return jsonify({'success': False, 'error': 'Not found (404)'}), 404
     return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def server_error(e):
-    if request.path.endswith('.json'):
-        return jsonify({'success': False, 'error': 'Server error (500)'}), 500
-    return render_template('500.html'), 500
