e543331e19975e6a634f1935c06c3ac69e967bbf
Andy Lulham <a.lulham@gmail.com>
2017-02-27 15:24:09 +0000

    Even more messing about with error handling

--- IATISimpleTester/views/error.py
+++ IATISimpleTester/views/error.py
@@ -1,18 +1,30 @@
 from os.path import join
 
 from flask import render_template, request, jsonify
+from werkzeug.exceptions import NotFound
 
 from IATISimpleTester import app
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    if request.path.endswith('.json'):
-        return jsonify({'success': False, 'error': 'Not found (404)'}), 404
-    return render_template(join('error', '404.html')), 404
+def not_found(e):
+    return generic_error(e)
 
 @app.errorhandler(403)
-def page_not_found(e):
+def access_denied(e):
+    return generic_error(e)
+
+@app.errorhandler(500)
+def server_error(e):
+    return generic_error(e)
+
+def generic_error(e):
+    name = '{name} ({code})'.format(
+        name=e.name,
+        code=e.code
+    )
+
     if request.path.endswith('.json'):
-        return jsonify({'success': False, 'error': 'Not found (403)'}), 403
-    return render_template(join('error', '403.html')), 403
+        return jsonify({'success': False, 'error': name}), e.code
+
+    return render_template('error.html', name=name), e.code
