cab4ec885d3101775ac16532d46f6f47700d1134
Andy Lulham <a.lulham@gmail.com>
2017-02-27 11:02:51 +0000

    Tidy up upload response a bit

--- IATISimpleTester/views/uploader.py
+++ IATISimpleTester/views/uploader.py
@@ -1,6 +1,6 @@
 import os.path
 
-from flask import request, jsonify, redirect, url_for
+from flask import abort, request, jsonify, redirect, url_for
 
 from IATISimpleTester import app, db
 from IATISimpleTester.models import SuppliedData
@@ -8,7 +8,6 @@ from IATISimpleTester.models import SuppliedData
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    resp = {}
     source_url = request.args.get('source_url')
     file = request.files.get('file')
     raw_text = request.args.get('paste')
@@ -21,17 +20,20 @@ def upload():
     elif file:
         form_name = 'upload_form'
 
-    if form_name:
-        data = SuppliedData(source_url, file, raw_text, form_name)
-        db.session.add(data)
-        db.session.commit()
+    if not form_name:
+        return abort(404)
+
+    data = SuppliedData(source_url, file, raw_text, form_name)
+    db.session.add(data)
+    db.session.commit()
+
+    if request.args.get('output') == 'json':
+        resp = {}
         resp['success'] = True
         resp['data'] = {
             'id': data.id,
             'original_file': data.original_file,
         }
-
-    if request.args.get('output') == 'json':
         return jsonify(resp)
 
     return redirect(url_for('explore', uuid=data.id))
