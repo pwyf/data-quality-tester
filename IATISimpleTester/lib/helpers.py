61ed75c0551ac140b0f04c2bbbe8b40ee07b5e8c
Andy Lulham <a.lulham@gmail.com>
2017-03-07 09:48:23 +0300

    Keep carriage returns in expressions

--- IATISimpleTester/lib/helpers.py
+++ IATISimpleTester/lib/helpers.py
@@ -21,5 +21,5 @@ def pprint(explanation):
     explanation = explanation.strip()
     if len(explanation) > 0:
         explanation = explanation[0].upper() + explanation[1:]
-    explanation += '.'
+    explanation = explanation.replace('\n', '<br>') + '.'
     return re.sub(r'`([^`]*)`', r'<code>\1</code>', explanation)
