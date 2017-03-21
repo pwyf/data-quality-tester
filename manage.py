505694c16fe15cda081fc4e853d71d5185f29e9d
Andy Lulham <a.lulham@gmail.com>
2017-03-21 13:26:45 +0000

    Make refresh-codelists a management command

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from IATISimpleTester import app, db
-from IATISimpleTester.scripts import FlushDataCommand
+from IATISimpleTester.scripts import FlushDataCommand, RefreshCodelistsCommand
 
 
 migrate = Migrate(app, db)
@@ -10,6 +10,7 @@ manager = Manager(app)
 manager.add_command('runserver', Server(threaded=True))
 manager.add_command('db', MigrateCommand)
 manager.add_command('flush-data', FlushDataCommand)
+manager.add_command('refresh-codelists', RefreshCodelistsCommand)
 
 if __name__ == '__main__':
     manager.run()
