1b0b51472a3f2fa8f71fe55bcf878d5ab01a40c9
Andy Lulham <a.lulham@gmail.com>
2017-11-08 23:09:52 +0000

    Tiny refactor

--- DataQualityTester/tasks.py
+++ DataQualityTester/tasks.py
@@ -8,20 +8,20 @@ from bdd_tester import bdd_tester
 @celery.task(bind=True)
 def test_file(self, path_to_file, feature_path, output_path):
     features = glob(join(feature_path, '**', '*.feature'), recursive=True)
-    total = len(features)
+    feature_count = len(features)
 
     overall_results = {}
     for idx, feature in enumerate(features):
-        results = bdd_tester(
+        result = bdd_tester(
             filepath=path_to_file,
             features=[feature],
             output_path=output_path
         )
-        overall_results.update(results)
+        overall_results.update(result)
         self.update_state(
             state='RUNNING',
             meta={
-                'progress': 100 * idx / total,
+                'progress': 100 * idx / feature_count,
                 'results': overall_results,
             }
         )
