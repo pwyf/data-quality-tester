59680a9bfaf604127aa23e44c557491ebcd2bf3b
Andy Lulham <a.lulham@gmail.com>
2017-11-17 23:11:38 +0000

    Remove a bunch of unused helpers

--- DataQualityTester/lib/helpers.py
+++ DataQualityTester/lib/helpers.py
@@ -1,20 +1,6 @@
 import re
 
 
-# given an expression list and the name of an expression,
-# select it,
-def select_expression(expression_list, expression_name,
-                      default_expression_name=None):
-    expression_dicts = {x["id"]: x for x in expression_list}
-    if expression_name not in expression_dicts:
-        expression_name = default_expression_name
-    return expression_name, expression_dicts.get(expression_name)
-
-
-def slugify(inp):
-    return inp.lower().replace(' ', '-')
-
-
 def pprint(explanation):
     explanation = explanation.strip()
     if len(explanation) > 0:
