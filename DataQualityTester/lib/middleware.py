f267e6bba40bd59e7d8ecde4bebe7b83dc02c66e
Michael Wood <michael.wood@opendataservices.coop>
2019-10-29 15:23:26 +0000

    middleware Don't pop the CSRF from the session
    We may need the value for future submissions. Fixes issue #41

--- DataQualityTester/lib/middleware.py
+++ DataQualityTester/lib/middleware.py
@@ -9,7 +9,7 @@ from DataQualityTester import app
 @app.before_request
 def csrf_protect():
     if request.method == 'POST':
-        token = session.pop('_csrf_token', None)
+        token = session.get('_csrf_token', None)
         if not token or token != request.form.get('_csrf_token'):
             abort(403)
 
