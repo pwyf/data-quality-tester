ec7859be6b0e8a2b319343849b7f18611876a1ec
Jared Parnell <jared.parnell@opendataservices.coop>
2019-12-05 16:19:42 +0000

    do not automatically run flask

--- vagrant/services.sh
+++ vagrant/services.sh
@@ -9,4 +9,4 @@ flask db upgrade
 
 redis-server > /dev/null 2>&1 &
 celery worker --app DataQualityTester.celery -l debug > /dev/null 2>&1 &
-flask run --host 0.0.0.0 > /dev/null 2>&1 &
+# flask run --host 0.0.0.0 > /dev/null 2>&1 &
